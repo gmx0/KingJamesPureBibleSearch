gmx0/KingJamesPureBibleSearch

commit b1370d728d1c3b0010b0db91db273dbf44304539

Switch to use resetModel instead of layoutChanged for width/offset changes. layoutChanged didn't work well because we were using row/col for the data instead of a pointer or ID for the data, meaning the selection model persistent indexes didn't work.  And after switching to using ID, it was extremely slow while it remapped the model.  And even then, it would crash when doing cursor moves because the currentSelection (i.e. cursor position) was invalid.  It's much quicker and more efficient to just change the entire model via a reset.

programs/ELSSearch/ELSSearchMainWindow.cpp
@@ -345,7 +345,7 @@ CELSSearchMainWindow::CELSSearchMainWindow(CBibleDatabasePtr pBibleDatabase,
 
 	connect(ui->spinWidth, SIGNAL(valueChanged(int)), this, SLOT(en_widthSpinValueChanged(int)));
 	connect(ui->spinOffset, SIGNAL(valueChanged(int)), m_pLetterMatrixTableModel, SLOT(setOffset(int)));
-	connect(m_pLetterMatrixTableModel, SIGNAL(layoutAboutToBeChanged(QList<QPersistentModelIndex>,QAbstractItemModel::LayoutChangeHint)), this, SLOT(en_letterMatrixLayoutAboutToChange()));
+	connect(m_pLetterMatrixTableModel, SIGNAL(modelAboutToBeReset()), this, SLOT(en_letterMatrixLayoutAboutToChange()));
 	connect(m_pLetterMatrixTableModel, SIGNAL(widthChanged(int)), this, SLOT(en_widthChanged(int)));
 	connect(m_pLetterMatrixTableModel, SIGNAL(offsetChanged(int)), this, SLOT(en_offsetChanged(int)));
 	connect(ui->cmbLetterCase, SIGNAL(currentIndexChanged(int)), this, SLOT(en_changedLetterCase(int)));
@@ -934,7 +934,7 @@ void CELSSearchMainWindow::en_changedSortOrder(int nIndex)
 
 // ----------------------------------------------------------------------------
 
-void CELSSearchMainWindow::en_letterMatrixLayoutAboutToChange()
+void CELSSearchMainWindow::en_letterMatrixModelAboutToBeReset()
 {
 	QRect rcTableView = ui->tvLetterMatrix->rect();
 	int nRow = ui->tvLetterMatrix->rowAt(rcTableView.height()/2);

programs/ELSSearch/ELSSearchMainWindow.h
@@ -122,7 +122,7 @@ protected slots:
 	// ----
 	void en_changedLetterCase(int nIndex);
 	// ----
-	void en_letterMatrixLayoutAboutToChange();
+	void en_letterMatrixModelAboutToBeReset();
 	void en_widthChanged(int nWidth);					// Width from ItemModel
 	void en_offsetChanged(int nOffset);
 	void en_widthSpinValueChanged(int nWidth);			// SpinBox value changed

programs/ELSSearch/LetterMatrixTableModel.cpp
@@ -355,9 +355,13 @@ void CLetterMatrixTableModel::setWidth(int nWidth)
 {
 	if (nWidth < 1) nWidth = 1;
 	if (m_nWidth != nWidth) {
-		emit layoutAboutToBeChanged();
+		// Use reset model instead of layout changed since current position
+		//	and such are invalidated too.  Even with the changes to use
+		//	our MatrixIndex for the model ID to where the indexes could get
+		//	remapped in the layout, it's really slow and clunky if we do that.
+		beginResetModel();
 		m_nWidth = nWidth;
-		emit layoutChanged();
+		endResetModel();
 		emit widthChanged(nWidth);
 	}
 }
@@ -367,9 +371,13 @@ void CLetterMatrixTableModel::setOffset(int nOffset)
 	if (nOffset < 0) nOffset = 0;
 	if (nOffset >= m_nWidth) nOffset = m_nWidth-1;
 	if (m_nOffset != nOffset) {
-		emit layoutAboutToBeChanged();
+		// Use reset model instead of layout changed since current position
+		//	and such are invalidated too.  Even with the changes to use
+		//	our MatrixIndex for the model ID to where the indexes could get
+		//	remapped in the layout, it's really slow and clunky if we do that.
+		beginResetModel();
 		m_nOffset = nOffset;
-		emit layoutChanged();
+		endResetModel();
 		emit offsetChanged(nOffset);
 	}
 }

commit ea77ac39cf894c4565b8bc15afdc692b8914fcae

Change matrix centering after layout change to use currentIndex. Since switching to reset model in b1370d728d1c3b0010b0db91db273dbf44304539, the currentIndex gets cleared anyway.  Users were already asking for a way to lock in a cell to center after changing the layout via width/offset.  This change to just use the currentIndex for centering and then set it after the layout change should be less confusing for more uesrs than adding cell locking. Fix the en_letterMatrixModelAboutToBeReset() connection that was broken in b1370d728d1c3b0010b0db91db273dbf44304539.

programs/ELSSearch/ELSSearchMainWindow.cpp
@@ -345,7 +345,7 @@ CELSSearchMainWindow::CELSSearchMainWindow(CBibleDatabasePtr pBibleDatabase,
 
 	connect(ui->spinWidth, SIGNAL(valueChanged(int)), this, SLOT(en_widthSpinValueChanged(int)));
 	connect(ui->spinOffset, SIGNAL(valueChanged(int)), m_pLetterMatrixTableModel, SLOT(setOffset(int)));
-	connect(m_pLetterMatrixTableModel, SIGNAL(modelAboutToBeReset()), this, SLOT(en_letterMatrixLayoutAboutToChange()));
+	connect(m_pLetterMatrixTableModel, SIGNAL(modelAboutToBeReset()), this, SLOT(en_letterMatrixModelAboutToBeReset()));
 	connect(m_pLetterMatrixTableModel, SIGNAL(widthChanged(int)), this, SLOT(en_widthChanged(int)));
 	connect(m_pLetterMatrixTableModel, SIGNAL(offsetChanged(int)), this, SLOT(en_offsetChanged(int)));
 	connect(ui->cmbLetterCase, SIGNAL(currentIndexChanged(int)), this, SLOT(en_changedLetterCase(int)));
@@ -936,21 +936,25 @@ void CELSSearchMainWindow::en_changedSortOrder(int nIndex)
 
 void CELSSearchMainWindow::en_letterMatrixModelAboutToBeReset()
 {
-	QRect rcTableView = ui->tvLetterMatrix->rect();
-	int nRow = ui->tvLetterMatrix->rowAt(rcTableView.height()/2);
-	int nCol = ui->tvLetterMatrix->columnAt(0) + m_pLetterMatrixTableModel->columnCount()/2;
-	m_nMatrixIndexToCenter = m_pLetterMatrixTableModel->matrixIndexFromRowCol(nRow, nCol);
+	m_nMatrixIndexToCenter = m_pLetterMatrixTableModel->matrixIndexFromModelIndex(ui->tvLetterMatrix->currentIndex());
 }
 
 void CELSSearchMainWindow::en_widthChanged(int nWidth)
 {
 	Q_UNUSED(nWidth);
-	if (m_nMatrixIndexToCenter) ui->tvLetterMatrix->scrollTo(m_pLetterMatrixTableModel->modelIndexFromMatrixIndex(m_nMatrixIndexToCenter), QAbstractItemView::PositionAtCenter);
+	if (m_nMatrixIndexToCenter) {
+		ui->tvLetterMatrix->scrollTo(m_pLetterMatrixTableModel->modelIndexFromMatrixIndex(m_nMatrixIndexToCenter), QAbstractItemView::PositionAtCenter);
+		ui->tvLetterMatrix->setCurrentIndex(m_pLetterMatrixTableModel->modelIndexFromMatrixIndex(m_nMatrixIndexToCenter));
+	}
 }
 
 void CELSSearchMainWindow::en_offsetChanged(int nOffset)
 {
 	Q_UNUSED(nOffset);
+	if (m_nMatrixIndexToCenter) {
+		ui->tvLetterMatrix->scrollTo(m_pLetterMatrixTableModel->modelIndexFromMatrixIndex(m_nMatrixIndexToCenter), QAbstractItemView::PositionAtCenter);
+		ui->tvLetterMatrix->setCurrentIndex(m_pLetterMatrixTableModel->modelIndexFromMatrixIndex(m_nMatrixIndexToCenter));
+	}
 }
 
 void CELSSearchMainWindow::en_widthSpinValueChanged(int nWidth)